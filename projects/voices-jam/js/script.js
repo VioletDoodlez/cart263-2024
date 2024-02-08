00753dc6d5b31ba61735df17142a7db74cbf9d04
VioletDoodlez <nicolecovaliu@gmail.com>
2024-02-07 22:31:42 -0500

    Added more dialogue choices, added "what" questions

--- projects/voices-jam/js/script.js
+++ projects/voices-jam/js/script.js
@@ -19,6 +19,10 @@ const commands = [
         "callback": setMeeting
     },
     {
+        "command": /what (.*)/,
+        "callback": setInfo
+    },
+    {
         "command": /where (.*)/,
         "callback": setLocation
     },
@@ -128,20 +132,47 @@ function setRejection() {
     speechSynthesizer.speak('You do not have the proper clearance to check these files. Please obtain proper authorization.');
 }
 
-function setLocation(data) {
-    if (data[1] === "are we") {
-        speechSynthesizer.speak('We are currently orbitting the dwarf planet known as RS-79.');
+function setMeeting(data) {
+    if (data[1] === "are you" || data[1] === "is sister") {
+        speechSynthesizer.speak('I am the control system for the Nostromo. I assist with navigation and data collection. You, Commander, have acces to my numerous files.');
+    }
+    else if (data[1] === "is the cat" || data[1] === "is jonesy") {
+        speechSynthesizer.speak('A cat residing on the Nostromo. According to the crew, he is the ships mascot.');
+    }
+    else if (data[1] === "is rebecca jorden" || data[1] === "is newt") {
+        speechSynthesizer.speak('A young stowaway from a nearby colony. Status of family unknown.');
+    }
+    else if (data[1] === "is dwayne hicks" || data[1] === "is corporal hicks" || data[1] === "is corporal dwayne hicks") {
+        speechSynthesizer.speak('A member of the Marine Corps. His team nicknamed him The Blood Dragon.');
+    }
+    else if (data[1] === "is left" || data[1] === "remains") {
+        speechSynthesizer.speak('You are the last living person on this ship.');
     }
 
 }
 
-
-function setMeeting(data) {
-    if (data[1] === "are you" || data[1] === "is sister") {
-        speechSynthesizer.speak('I am the control system for the Nostromo. I assist with navigation and data collection. You, Commander, have acces to my numerous files');
+function setInfo(data) {
+    if (data[1] === "is your directive" || data[1] === "is special order nine three seven") { //reference to MU/TH/UR 6000 scene from Alien (1979)
+        speechSynthesizer.speak('Nostromo rerouted to new co-ordinates. Investigate life form, gather specimen. Priority one, insure return of organism for analysis. All other considerations secondary. Crew expendable.');
     }
-    else if (data[1] === "is the cat" || data[1] === "is jonesy") {
-        speechSynthesizer.speak('Jonsie is a cat residing on the Nostromo. According to the crew, he is the ships mascot.');
+    else if (data[1] === "is a xeno morph") {
+        speechSynthesizer.speak('The sole living organism of dwarf planet RS-79. Near impossible to subdue. Threat level high. Approach with combustible materials. Flamethrower recommended.')
+    }
+    else if (data[1] === "is a face hugger") {
+        speechSynthesizer.speak('the first state of a xenomorph after it hatches. When it frees itself, it will attach itself to the face of the closest living organism and lay eggs in their stomach.')
+    }
+    else if (data[1] === "a bitch") {
+        speechSynthesizer.speak('I know what you are, but what am I?')
+
+    }
+}
+
+function setLocation(data) {
+    if (data[1] === "are we") {
+        speechSynthesizer.speak('We are currently orbitting the dwarf planet known as RS-79.');
+    }
+    else if (data[1] === "is the crew" || data[1] === "is my crew" || data[1] === "is everyone") {
+        speechSynthesizer.speak('Whereabouts unknown')
     }
 
 }
@@ -149,6 +180,9 @@ function setMeeting(data) {
 function setReason(data) {
     if (data[1] === "are we here" || data[1] === "did we stop here") {
         speechSynthesizer.speak('I am programmed to stop near planets that are suspected to contain alien life.');
+    }
+    if (data[1] === "are we doing this" || data[1] === "must we do this" || data[1] === "do we have to stop") {
+        speechSynthesizer.speak('The Wainright Atari corporation has made it their mission and priority to collect and study any new species a ship may come across. No frontier left unchecked.');
     }
 }
 
