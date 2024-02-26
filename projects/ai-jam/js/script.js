bec8beaa751ba12d2124096661f9354a70b43272
VioletDoodlez <nicolecovaliu@gmail.com>
2024-02-25 22:27:50 -0500

    Added instructions, made instructions disappear when clicked, changed title in html

--- projects/ai-jam/js/script.js
+++ projects/ai-jam/js/script.js
@@ -2,8 +2,7 @@
  * AI Jam
  * Nicole Covaliu
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Object recognition game where the player must guess what the 
  */
 
 "use strict";
@@ -20,84 +19,90 @@ let cocossd;
 let predictions = [];
 
 let objects = [
-
     {
         label: "cell phone",
-        description: "Communication box"
+        description: "Communication box",
+        found: false
     },
     // {
     //     label: "keyboard",
-    //     description: "Machine communicatior"
+    //     description: "Machine communicatior",
+    // found: false
     // },
     // {
     //     label: "mouse",
-    //     description: "Machine animal"
+    //     description: "Machine animal",
+    // found: false
     // },
     {
         label: "bottle",
-        description: "Liquid tube"
+        description: "Liquid tube",
+        found: false
     },
     // {
     //     label: "cup",
-    //     description: "Liquid holder"
+    //     description: "Liquid holder",
+    // found: false
     // },
     // {
     //     label: "bowl",
-    //     description: "Food retainer"
+    //     description: "Food retainer",
+    // found: false
     // },
     // {
     //     label: "spoon",
-    //     description: "Scooping device"
+    //     description: "Scooping device",
+    // found: false
     // },
     // {
     //     label: "fork",
-    //     description: "Stabbing device"
+    //     description: "Stabbing device",
+    // found: false
     // },
     // {
     //     label: "knife",
-    //     description: "Slicing device"
+    //     description: "Slicing device",
+    // found: false
     // },
     // {
     //     label: "apple",
-    //     description: "Red food-ball"
+    //     description: "Red food-ball",
+    // found: false
     // },
     // {
     //     label: "banana",
-    //     description: "Yellow food-tube"
+    //     description: "Yellow food-tube",
+    // found: false
     // },
     {
         label: "book",
-        description: "Keeper of Knowledge"
+        description: "Keeper of Knowledge",
+        found: false
     },
     {
         label: "scissors",
-        description: "Separator"
+        description: "Separator",
+        found: false
     },
     // {
     //     label: "teddy bear",
-    //     description: "Fake animal"
+    //     description: "Fake animal",
+    // found: false
+
     // },
     // {
     //     label: "toothbrush",
-    //     description: "Tusk cleaner"
+    //     description: "Tusk cleaner",
+    // found: false
     // },
 ];
 
+// list of objects to find
 let currentObject1 = undefined;
 let currentObject2 = undefined;
 let currentObject3 = undefined;
 
-let desc = {
-    fill: {
-        r1: 255,
-        g1: 0,
-        r2: 255,
-        g2: 0,
-        r3: 255,
-        g3: 0,
-        b: 0,
-    }
-}
+let alpha = 255;
 
 /**
 Starts the webcam and the ObjectDetector
@@ -192,46 +197,126 @@ function running() {
     // Display the webcam
     image(video, 0, 0, width, height);
 
+    // Display list
     push();
     textSize(22);
     textStyle(BOLD);
     textAlign(TOP, LEFT);
+    fill(0)
     text(`Find:`, width / 5 - 75, height / 5 - 70);
     pop();
 
     push();
     textSize(22);
     textAlign(TOP, LEFT);
-    fill(desc.fill.r1, desc.fill.g1, desc.fill.b)
+    if (currentObject1.found) {
+        fill(0, 255, 0); // text colour changes green if found
+    }
+    else {
+        fill(255, 0, 0);
+    }
     text(currentObject1.description, width / 5 - 75, height / 5 - 50);
     pop();
 
     push();
     textSize(22);
     textAlign(TOP, LEFT);
-    fill(desc.fill.r2, desc.fill.g2, desc.fill.b)
+    if (currentObject2.found) {
+        fill(0, 255, 0); // text colour changes green if found
+    }
+    else {
+        fill(255, 0, 0);
+    }
     text(currentObject2.description, width / 5 - 75, height / 5 - 30);
     pop();
 
     push();
     textSize(22);
     textAlign(TOP, LEFT);
-    fill(desc.fill.r3, desc.fill.g3, desc.fill.b)
+    if (currentObject3.found) {
+        fill(0, 255, 0); // text colour changes green if found
+    }
+    else {
+        fill(255, 0, 0);
+    }
     text(currentObject3.description, width / 5 - 75, height / 5 - 10);
     pop();
 
-    // rectMode(CORNER);
-    // fill(0);
-    // rect(440, 280, 200, 200);
+    rectMode(CORNER);
+    noStroke();
+    fill(0, 0, 0, alpha);
+    rect(290, 130, 350, 350);
 
-    // push();
-    // textSize(22);
-    // textStyle(BOLD);
-    // fill(255);
-    // textAlign(TOP, LEFT);
-    // text(`Find:`, 440, 280);
-    // pop();
+    push();
+    textSize(22);
+    textStyle(BOLD);
+    fill(255, 255, 255, alpha);
+    textAlign(CENTER, CENTER);
+    text(`Greeting Human!`, 465, 150);
+    pop();
 
+    push();
+    textSize(12);
+    fill(255, 255, 255, alpha);
+    textAlign(CENTER, CENTER);
+    text(`We are what you call "extraterrestrials" and we need help!`, 465, 180);
+    pop();
+
+    push();
+    textSize(12);
+    fill(255, 255, 255, alpha);
+    textAlign(CENTER, CENTER);
+    text(`We are researching your planet and`, 465, 220);
+    pop();
+
+    push();
+    textSize(12);
+    fill(255, 255, 255, alpha);
+    textAlign(CENTER, CENTER);
+    text(`we need to know how certain objects look.`, 465, 240)
+    pop();
+
+    push();
+    textSize(12);
+    fill(255, 255, 255, alpha);
+    textAlign(CENTER, CENTER);
+    text(`A list of objects are at the top left corner of your screen.`, 465, 280);
+    pop();
+
+    push();
+    textSize(12);
+    fill(255, 255, 255, alpha);
+    textAlign(CENTER, CENTER);
+    text(`Find these objects in your residence and `, 465, 320);
+    pop();
+
+    push();
+    textSize(12);
+    fill(255, 255, 255, alpha);
+    textAlign(CENTER, CENTER);
+    text(`show them to your device's camera.`, 465, 340)
+    pop();
+
+    push();
+    textSize(12);
+    fill(255, 255, 255, alpha);
+    textAlign(CENTER, CENTER);
+    text(`BE WARNED: We don't know the objects' proper names!`, 465, 380);
+    pop();
+
+    push();
+    textSize(12);
+    fill(255, 255, 255, alpha);
+    textAlign(CENTER, CENTER);
+    text(`You'll have to guess.`, 465, 400)
+    pop();
+
+    push();
+    textSize(12);
+    fill(255, 255, 255, alpha);
+    textAlign(CENTER, CENTER);
+    text(`(click to remove)`, 465, 440);
+    pop();
 
     // Check if there currently predictions to display
     if (predictions) {
@@ -244,21 +329,18 @@ function running() {
                 highlightObject(object);
             }
             if (predictions[i].label === currentObject1.label) {
-                desc.fill.r1 = desc.fill.r1 - 255;
-                desc.fill.g1 = desc.fill.g1 + 255;
+                currentObject1.found = true;
             }
 
             if (predictions[i].label === currentObject2.label) {
-                desc.fill.r2 = desc.fill.r2 - 255;
-                desc.fill.g2 = desc.fill.g2 + 255;
+                currentObject2.found = true;
             }
 
             if (predictions[i].label === currentObject3.label) {
-                desc.fill.r3 = desc.fill.r3 - 255;
-                desc.fill.g3 = desc.fill.g3 + 255;
+                currentObject3.found = true;
             }
 
-            if (desc.fill.g1 === 255 && desc.fill.g2 === 255 && desc.fill.g3 === 255) {
+            if (currentObject1.found && currentObject2.found && currentObject3.found) {
                 state = `end`;
             }
         }
@@ -272,6 +354,7 @@ function end() {
     textSize(32);
     textStyle(BOLD);
     textAlign(CENTER, CENTER);
+    fill(255);
     text(`You found all the objects!`, width / 2, height / 2);
     pop();
 
@@ -279,7 +362,8 @@ function end() {
     textSize(22);
     textStyle(BOLD);
     textAlign(CENTER, CENTER);
-    text(`Thank you! Now all we need is a live human!`, width / 2, height / 2);
+    fill(255);
+    text(`Thank you! Now all we need is a live human!`, width / 2, height / 2 + 42);
     pop();
 }
 
@@ -303,6 +387,9 @@ function highlightObject(object) {
     pop();
 }
 
-// function mousePressed() {
-//
-// }
+function mousePressed() {
+    let d = dist(415, 200, mouseX, mouseY)
+    if (d < 350) {
+        alpha = alpha - 255;
+    }
+}
