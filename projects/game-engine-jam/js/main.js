c094daf9ad77ea7df727202e9650a219d27c97a7
VioletDoodlez <nicolecovaliu@gmail.com>
2024-03-04 22:20:39 -0500

    Renamed script.js to main.js, added new game object

--- projects/game-engine-jam/js/main.js
+++ projects/game-engine-jam/js/main.js
@@ -8,6 +8,19 @@
 
 "use strict";
 
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+
+    physics: {
+        default: 'arcade',
+    },
+    scene: []
+};
+
+let game = new Phaser.Game(config);
+
 /**
  * Description of preload
 */
