590ab952508e5ba4d6bf2e286c574402032bc5da
VioletDoodlez <nicolecovaliu@gmail.com>
2024-03-14 13:09:58 -0400

    Avatar flips depending on direction

--- projects/game-engine-jam/js/Play.js
+++ projects/game-engine-jam/js/Play.js
@@ -9,7 +9,7 @@ class Play extends Phaser.Scene {
         this.width = this.game.canvas.width * 2;
         this.height = this.game.canvas.height;
         this.physics.world.setBounds(0, 0, this.width, this.height);
-        this.background = this.add.tileSprite(this.width / 2, this.height / 2 + 50, this.width, this.height, 'bg');
+        this.background = this.add.tileSprite(this.width / 2, this.height / 2, this.width, this.height, 'bg');
 
         this.avatar = this.physics.add.sprite(200, 350, `avatar`);
         this.avatar.setCollideWorldBounds(true);
@@ -17,7 +17,7 @@ class Play extends Phaser.Scene {
         this.avatar.play(`idle`);
 
         this.cameras.main.startFollow(this.avatar, true, 1, 1);
-        this.cameras.main.setBounds(0, 0, this.width, this.width);
+        this.cameras.main.setBounds(0, 0, this.width, this.height);
 
         this.cursors = this.input.keyboard.createCursorKeys();
     }
@@ -56,9 +56,11 @@ class Play extends Phaser.Scene {
     handleInput() {
         if (this.cursors.left.isDown) {
             this.avatar.setVelocityX(-200);
+            this.avatar.flipX = true;
         }
         else if (this.cursors.right.isDown) {
             this.avatar.setVelocityX(200);
+            this.avatar.flipX = false;
         }
         else {
             this.avatar.setVelocityX(0);
